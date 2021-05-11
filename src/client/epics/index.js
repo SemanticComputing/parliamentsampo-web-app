42a07d76f91558a610bd1cdee84593b879fe8388
esikkala <esko.ikkala@aalto.fi>
2021-05-11 12:59:06 +0300

    Set up translations

--- src/client/epics/index.js
+++ src/client/epics/index.js
@@ -11,8 +11,8 @@ import {
 } from 'rxjs/operators'
 import { combineEpics, ofType } from 'redux-observable'
 import intl from 'react-intl-universal'
-import localeEN from '../translations/sampo/localeEN.json'
-import localeFI from '../translations/sampo/localeFI.json'
+import localeEN from '../translations/ps/localeEN.json'
+import localeFI from '../translations/ps/localeFI.json'
 import { stateToUrl, pickSelectedDatasets } from '../helpers/helpers'
 import querystring from 'querystring'
 import {
