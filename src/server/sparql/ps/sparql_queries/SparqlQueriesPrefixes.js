a89c6c9929117677c5b2cf7c5b6398aaa8e88eec
Petri Leskinen <petri.leskinen@aalto.fi>
2021-09-22 16:43:57 +0300

    Fix showing electoral terms

--- src/server/sparql/ps/sparql_queries/SparqlQueriesPrefixes.js
+++ src/server/sparql/ps/sparql_queries/SparqlQueriesPrefixes.js
@@ -6,16 +6,16 @@ export const prefixes = `
   PREFIX frbroo: <http://erlangen-crm.org/efrbroo/>
   PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
   PREFIX geosparql: <http://www.opengis.net/ont/geosparql#>
-  PREFIX gvp: <http://vocab.getty.edu/ontology#>
   PREFIX gn: <http://www.geonames.org/ontology#>
+  PREFIX gvp: <http://vocab.getty.edu/ontology#>
   PREFIX owl: <http://www.w3.org/2002/07/owl#>
   PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
   PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
   PREFIX sch: <http://schema.org/>
-  PREFIX spatial: <http://jena.apache.org/spatial#>
   PREFIX sd: <http://www.w3.org/ns/sparql-service-description#>
-  PREFIX semparls: <http://ldf.fi/schema/semparl/>
   PREFIX semparl_linguistics: <http://ldf.fi/schema/semparl/linguistics/>
+  PREFIX semparls: <http://ldf.fi/schema/semparl/>
+  PREFIX spatial: <http://jena.apache.org/spatial#>
   PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
   PREFIX text: <http://jena.apache.org/text#>
   PREFIX wgs84: <http://www.w3.org/2003/01/geo/wgs84_pos#>
