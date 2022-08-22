81f0adc3f2d7ddd6740b000279647c9bcb8a6820
Petri Leskinen <petri.leskinen@aalto.fi>
2022-08-22 15:24:43 +0300

    Added input for full text search

--- src/server/sparql/ps/sparql_queries/SparqlQueriesFullText.js
+++ src/server/sparql/ps/sparql_queries/SparqlQueriesFullText.js
@@ -1,79 +1,85 @@
 export const fullTextSearchProperties = `
-  {
-    VALUES ?type__id {
-      frbroo:F4_Manifestation_Singleton 
-      frbroo:F1_Work
-      frbroo:F2_Expression
-      crm:E10_Transfer_of_Custody
-      crm:E12_Production
-      crm:E7_Activity
-      crm:E67_Birth 
-      crm:E69_Death 
-      mmm-schema:ManuscriptActivity
-      crm:E21_Person 
-      crm:E74_Group 
-      crm:E39_Actor
-      crm:E53_Place
-      crm:E78_Collection
-    }
-    ?id a ?type__id .
-    ?type__id skos:prefLabel|rdfs:label ?type__prefLabel . 
-  }
-  UNION
-  {
-    ?id crm:P3_has_note ?note . # crm:P3_has_note has been added to text index
-  }
-  UNION
-  {
-    ?id a frbroo:F4_Manifestation_Singleton .
-    ?id skos:prefLabel ?prefLabel__id .
-    BIND(?prefLabel__id as ?prefLabel__prefLabel)
-    BIND(CONCAT("/manuscripts/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
-  }
-  UNION
-  {
-    ?id a frbroo:F1_Work .
-    ?id skos:prefLabel ?prefLabel__id .
-    BIND(?prefLabel__id as ?prefLabel__prefLabel)
-    BIND(CONCAT("/works/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
-  }
-  UNION
-  {
-    VALUES ?eventClass { crm:E10_Transfer_of_Custody crm:E12_Production crm:E7_Activity crm:E67_Birth crm:E69_Death mmm-schema:ManuscriptActivity }
-    ?id a ?eventClass .
-    OPTIONAL { ?id skos:prefLabel ?prefLabel__id_ }
-    BIND(COALESCE(?prefLabel__id_, ?id) as ?prefLabel__id)
-    BIND(?prefLabel__id as ?prefLabel__prefLabel)
-    BIND(CONCAT("/events/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
-  }
-  UNION
-  {
-    VALUES ?actorClass { crm:E21_Person crm:E74_Group crm:E39_Actor }
-    ?id a ?actorClass .
-    ?id skos:prefLabel ?prefLabel__id .
-    BIND(?prefLabel__id as ?prefLabel__prefLabel)
-    BIND(CONCAT("/actors/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
-  }
-  UNION
-  {
-    ?id a crm:E53_Place .
-    OPTIONAL { ?id skos:prefLabel ?prefLabel__id_ }
-    BIND(COALESCE(?prefLabel__id_, ?id) as ?prefLabel__id)
-    BIND(?prefLabel__id as ?prefLabel__prefLabel)
-    BIND(CONCAT("/places/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
-  }
-  UNION
-  {
-    ?id a crm:E78_Collection .
-    ?id skos:prefLabel ?prefLabel__id .
-    BIND(?prefLabel__id as ?prefLabel__prefLabel)
-    BIND(CONCAT("/collections/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
-  }
-  UNION
-  {
-    ?id a frbroo:F2_Expression .
-    ?id skos:prefLabel ?prefLabel__id .
-    BIND(?prefLabel__id as ?prefLabel__prefLabel)
-    BIND(CONCAT("/expressions/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
-  }
+{
+VALUES ?type__id {
+  crm:E53_Place
+  bioc:Person
+  semparls:ElectoralDistrict
+  semparls:Government
+  semparls:MunicipalCouncil
+  semparls:Party
+  semparls:Publication
+  semparls:Organization
+  semparls:School
+}
+?id a ?type__id .
+?type__id skos:prefLabel ?type__prefLabel . 
+FILTER(LANG(?type__prefLabel) = 'fi')
+
+{
+  ?id a bioc:Person ;
+    skos:prefLabel ?prefLabel__id .
+  BIND(?prefLabel__id as ?prefLabel__prefLabel)
+  BIND(CONCAT("/people/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+}
+UNION
+{
+  ?id a crm:E53_Place ; skos:prefLabel ?prefLabel__id .
+  FILTER NOT EXISTS { ?id semparls:has_duplicate_child [] }
+  BIND(?prefLabel__id as ?prefLabel__prefLabel)
+  BIND(CONCAT("/places/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+}
+UNION 
+{
+  ?id a semparls:ElectoralDistrict ; skos:prefLabel ?prefLabel__id .
+  FILTER (lang(?prefLabel__id)="fi")
+  BIND(?prefLabel__id as ?prefLabel__prefLabel)
+  BIND(CONCAT("/districts/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+}
+UNION 
+{
+  ?id a semparls:Government ; skos:prefLabel ?prefLabel__id .
+  FILTER (lang(?prefLabel__id)="fi")
+  BIND(?prefLabel__id as ?prefLabel__prefLabel)
+  BIND(CONCAT("/groups/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+}
+UNION 
+{
+  ?id a semparls:MunicipalCouncil ; skos:prefLabel ?prefLabel__id .
+  FILTER (lang(?prefLabel__id)="fi")
+  BIND(?prefLabel__id as ?prefLabel__prefLabel)
+  BIND(CONCAT("/groups/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+}
+UNION 
+{
+  ?id a semparls:Organization ; skos:prefLabel ?prefLabel__id .
+  FILTER (lang(?prefLabel__id)="fi")
+  BIND(?prefLabel__id as ?prefLabel__prefLabel)
+  BIND(CONCAT("/groups/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+}
+UNION 
+{
+  ?id a semparls:Party ; skos:prefLabel ?prefLabel__id .
+  FILTER (lang(?prefLabel__id)="fi")
+  BIND(?prefLabel__id as ?prefLabel__prefLabel)
+  BIND(CONCAT("/groups/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+}
+UNION 
+{
+  ?id a semparls:School ; skos:prefLabel ?prefLabel__id .
+  FILTER (lang(?prefLabel__id)="fi")
+  BIND(?prefLabel__id as ?prefLabel__prefLabel)
+  BIND(CONCAT("/groups/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+}
+}
+UNION
+{
+  VALUES ?sclass { bioc:Occupation }
+  ?id a rdfs:Class ; rdfs:subClassOf ?sclass ; skos:prefLabel ?prefLabel__id .
+  FILTER (lang(?prefLabel__id)="fi")
+  BIND(?prefLabel__id as ?prefLabel__prefLabel)
+  BIND(CONCAT("/occupations/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+
+  ?sclass skos:prefLabel ?type__prefLabel .
+  FILTER(LANG(?type__prefLabel) = 'fi')
+}
   `
