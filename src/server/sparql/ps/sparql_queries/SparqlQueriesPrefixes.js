3e6932a400e627468709383c96e39a039e2ca6f7
HeikkiR <heikki.rantala@aalto.fi>
2024-12-18 11:40:33 +0200

    Update texts

--- src/server/sparql/ps/sparql_queries/SparqlQueriesPrefixes.js
+++ src/server/sparql/ps/sparql_queries/SparqlQueriesPrefixes.js
@@ -23,5 +23,4 @@ export const prefixes = `
   PREFIX xl: <http://www.w3.org/2008/05/skos-xl#>
   PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
   PREFIX facets: <http://ldf.fi/schema/parliamentsampo-portal/>
-
 `
